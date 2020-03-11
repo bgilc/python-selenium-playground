e19279e7637a21d244869d79a3fe402c78d0c3b7
Tomasz Lehmann <19573104+bgilc@users.noreply.github.com>
2020-03-11 21:07:09 +0100

    Skeleton.

---
+++ functions.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+class Common():
+
+    def __init__(self):
+        self.driver = webdriver.chrome
+
+class Actions(Common):
+    pass
---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+from functions import *
+
+class TestCases(unittest.TestCase):
+
+    def setUp(self):
+        self.action = Actions()
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
