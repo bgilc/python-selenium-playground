8ee9e7284d984465563babb4fba83708bcc0070f
Tomasz Lehmann <19573104+bgilc@users.noreply.github.com>
2020-03-12 17:32:56 +0100

    Download file test added, minor changes.

--- functions.py
+++ functions.py
@@ -1,4 +1,11 @@
+import hashlib
+import time
+import os.path
 from selenium import webdriver
+from selectors import *
+
+downloaded_file_hash = 'e2ad0e90d464b9ac9cb2854b34505d91a302249b152ff716e877933736ae0ab8212b97a831845586d7383bb2bbe60e09e29150099927445ce8c912285f900ed3'
+main_page_the_internet_herokuapp = 'https://the-internet.herokuapp.com'
 
 class Common():
 
@@ -6,4 +13,23 @@ class Common():
         self.driver = webdriver.Chrome()
 
 class Actions(Common):
-    pass
+
+    def download_file(self):
+        self.driver.get(main_page_the_internet_herokuapp)
+        self.driver.find_element_by_xpath(file_download_page).click()
+        self.driver.find_element_by_xpath(file_to_download).click()
+
+    def downloaded_file_check_hash(self):
+        with open(r'C:\Users\sion\Downloads\quality-assurance1.jpg', 'rb') as file_object:
+            file = file_object.read()
+        return hashlib.sha3_512(file).hexdigest()
+
+    def does_file_exist(self):
+        seconds_waited = 0
+        while seconds_waited < 60 and os.path.isfile(r'C:\Users\sion\Downloads\quality-assurance1.jpg') == False:
+            time.sleep(1)
+            seconds_waited += 1
+
+    def search_for_typo_page_go_to(self):
+        self.driver.get(main_page_the_internet_herokuapp)
+        self.driver.find_element_by_xpath(typo_page).click()
--- tests.py
+++ tests.py
@@ -10,8 +10,13 @@ class TestCases(unittest.TestCase):
     def tearDown(self):
         self.action.driver.quit()
 
+    def test_download_file(self):
+        self.action.download_file()
+        self.action.does_file_exist()
+        self.assertEqual(downloaded_file_hash, self.action.downloaded_file_check_hash(), 'Wrong hash!')
+
     def test_search_for_typo(self):
-        self.action.driver.get('http://the-internet.herokuapp.com/typos')
+        self.action.search_for_typo_page_go_to()
         self.assertEqual('This example demonstrates a typo being introduced. It does it randomly on each page load.', self.action.driver.find_element_by_xpath(text_to_check_first_line).get_attribute('outerText'), 'Typo detected!')
         self.assertEqual("Sometimes you'll see a typo, other times you won't.", self.action.driver.find_element_by_xpath(text_to_check_second_line).get_attribute('outerText'), 'Typo detected!')
 
